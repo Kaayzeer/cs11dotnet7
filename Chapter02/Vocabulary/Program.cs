5d0422dd7e1ee26c792494d51fe31aceb0a08de6
Fadil Al <Fadil.alfarhan@gmail.com>
2023-09-26 08:59:19 +0200

    pull request

--- Chapter02/Vocabulary/Program.cs
+++ Chapter02/Vocabulary/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-// #error version
+﻿using System.Reflection;
+
+Assembly ? myApp = Assembly.GetEntryAssembly();
+
+if (myApp == null) return;
+
+foreach (AssemblyName name in myApp.GetReferencedAssemblies())
+{
+    Assembly a = Assembly.Load(name);
+    int methodCount = 0;
+    foreach (TypeInfo t in a.DefinedTypes)
+    {
+        methodCount += t.GetMethods().Count();
+    }
+    Console.WriteLine(
+        "{0:N0} types with {1:N0} method in {2} assembly.",
+        arg0: a.DefinedTypes.Count(),
+        arg1: methodCount,
+        arg2: name.Name
+
+        );
+}
