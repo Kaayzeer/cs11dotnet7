4ee2e463bc5387e6529682d07d22940ac8be4729
FaaDiiL <fadil.alfarhan@gmail.com>
2023-10-01 14:20:59 +0200

    Understanding iteration statements. Looping with the while statement. Looping with the do statement. Looping with the for statement Looping with the foreach statement

---
+++ Chapter03/IterationStatements/Program.cs
@@ -0,0 +1,42 @@
+﻿int x = 0;
+while (x < 10)
+{
+    WriteLine(x);
+    x++;
+}
+
+// Looping with the do statement
+
+string? actualPassword = "Pa$$w0rd";
+string? password;
+int maximumAttempts = 10;
+int attempts = 0;
+
+do
+{
+    attempts++;
+    Write("Enter your password: ");
+    password = ReadLine();
+}
+while ((password != actualPassword) & (attempts < maximumAttempts));
+
+if (password == actualPassword)
+{
+    WriteLine("Correct!");
+}
+else
+{
+    WriteLine("You have used {0} attempts! The password was {1}.",
+        arg0: maximumAttempts, arg1: actualPassword);
+}
+
+for (int y = 1; y <= 10; y++)
+{
+    WriteLine(y);
+}
+
+string[] names = { "Adam", "Barry", "charlie"};
+foreach (string name in names)
+{
+    WriteLine($"{name} has {name.Length} characters.");
+}
