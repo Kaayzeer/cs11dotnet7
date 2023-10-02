ab6344413fb91becbffb86028a707a64fe3cdfd8
FaaDiiL <fadil.alfarhan@gmail.com>
2023-10-02 10:38:15 +0200

    Throwing overflow exceptions with the checked statement Disabling compiler overflow checks with the unchecked statement

---
+++ Chapter03/CheckingForOverflow/Program.cs
@@ -0,0 +1,27 @@
+﻿try
+{
+    checked
+    {
+        var x = int.MaxValue - 1;
+        WriteLine($"Initial value: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+        x++;
+        WriteLine($"After incrementing: {x}");
+    }
+}
+catch (OverflowException)
+{
+    WriteLine("The code overflowed but I caught the exception.");
+}
+unchecked
+{
+    int y = int.MaxValue + 1;
+    WriteLine($"Initial value: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+    y--;
+    WriteLine($"After decrementing: {y}");
+}
