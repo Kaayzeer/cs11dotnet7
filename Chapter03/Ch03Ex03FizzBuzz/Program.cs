89d205b8d12c93b7b6fb99c75be53e368db03e11
FaaDiiL <fadil.alfarhan@gmail.com>
2023-10-03 06:52:14 +0200

    Exercise 3.3 – Practice loops and operators create FizzBuzz game.

---
+++ Chapter03/Ch03Ex03FizzBuzz/Program.cs
@@ -0,0 +1,32 @@
+﻿const byte max = 100;
+
+for (byte i =1 ; i <= max; i++)
+{
+    if (i % 15 == 0)
+    {
+        Write("FizzBuzz");
+    }
+    else if (i % 5 == 0)
+    {
+        Write("Buzz");
+    }
+    else if (i % 3 == 0)
+    {
+        Write("Fizz");
+    }
+    else
+    {
+        Write($"{i}");
+    }
+
+    if (i < max)
+    {
+        Write(", ");
+    }
+
+    if (i % 10 == 0)
+    {
+        WriteLine();
+    }
+}
+WriteLine();
