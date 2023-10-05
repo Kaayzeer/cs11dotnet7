313335836fae7bf3123ab54028600ff153b79746
FaaDiiL <fadil.alfarhan@gmail.com>
2023-10-05 10:11:07 +0200

    Debugging during development Creating code with a deliberate bug Setting a breakpoint and starting debugging Navigating with the debugging toolbar Debugging windows Stepping through code Customizing breakpoints

---
+++ Chapter04/Debugging/Program.cs
@@ -0,0 +1,13 @@
+﻿double a = 4.5;
+double b = 2.5;
+double answear = Add(a, b);
+
+WriteLine($"{a} + {b} = {answear}");
+WriteLine("Press ENTER to end the app.");
+ReadLine(); // wait for press ENTER
+
+
+double Add(double a, double b)
+{
+    return a + b; // deliberate bug!
+}
